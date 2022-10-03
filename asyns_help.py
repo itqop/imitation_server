3292d5277fbce56348ba0fb1e333edab46263188
itqop <leonkl32@gmail.com>
2022-10-03 05:05:20 +0300

    coplete with generation json snapshots

--- asyns_help.py
+++ asyns_help.py
@@ -1,16 +1,16 @@
 from db_commands import generate_json_dumps
 from tcp_serv import tcp_start
 import asyncio
-import logging
 
 
-async def start(interval):
-    logging.info(msg="Starting generate json dumps..")
+async def start(interval: int):
+    """
+    Function for asynchronous launch of tasks.
+    :param interval: json data sending interval time !IN SECONDS!
+    :return: -async-
+    """
     task_json = asyncio.create_task(generate_json_dumps(interval))
-    logging.info(msg="Starting tcp server..")
     task_tcp_serv = asyncio.create_task(tcp_start())
 
     await task_json
     await task_tcp_serv
-
-
--- tcp_serv.py
+++ tcp_serv.py
@@ -1,7 +1,28 @@
 import asyncio
+import datetime
+import pathlib
+import json
+import logging
 
 
 async def tcp_start():
+    """
+    ?
+    :return: -async-
+    """
+    logging.info(msg="Starting tcp server..")
+    await asyncio.sleep(1)
+    path = pathlib.Path.cwd() / 'json_cache'
     while True:
-        await asyncio.sleep(1)
-        print("tcp")
+        files = path.glob("*.json")
+        for p in files:
+            with open(p) as json_file:
+                data = json.load(json_file)
+                data["timestamp_push"] = int(datetime.datetime.now().timestamp())
+            with open(p, "w") as outfile:
+                json.dump(data, outfile)
+            logging.info(msg="Send " + p.name)
+            await asyncio.sleep(1)
+            p.unlink()
+            logging.info(msg="Deleting " + p.name)
+        await asyncio.sleep(10)
